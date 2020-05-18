9167214bfb64531172bd768a932525f377ea27c7
Jędrzej Maćkowiak <j.mackowiak11@gmail.com>
2020-05-18 23:46:44 +0200

    Updates

---
+++ precache-manifest.8b2a3cf0ea6ba2b4767111d1a208f549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecf452cbde3799ee5ed326322d2f01c",
+    "url": "/Songbook-Manager-API-front/index.html"
+  },
+  {
+    "revision": "f2a8647c54069bab1ba1",
+    "url": "/Songbook-Manager-API-front/static/css/main.c407dd2e.chunk.css"
+  },
+  {
+    "revision": "bb1f4b9f9e4c8647e872",
+    "url": "/Songbook-Manager-API-front/static/js/2.85ab2288.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Songbook-Manager-API-front/static/js/2.85ab2288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a8647c54069bab1ba1",
+    "url": "/Songbook-Manager-API-front/static/js/main.5e50199c.chunk.js"
+  },
+  {
+    "revision": "3312c8431933f2858af9",
+    "url": "/Songbook-Manager-API-front/static/js/runtime-main.5e05ffb8.js"
+  }
+]);
